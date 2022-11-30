72470c5077d19d3b97aa627d9717b1b75140126c
Gustavo Rocha <gustavorocha11@gmail.com>
2022-11-30 10:01:22 -0300

    crate and initialize project

---
+++ backend/src/resolvers/UserResolver.ts
@@ -0,0 +1,14 @@
+import { Mutation, Query, Resolver } from "type-graphql";
+import { User } from "../models/User";
+
+@Resolver()
+export class UserResolver {
+    private data: User[] = [];
+
+    @Query(() => [User])
+    async users() {
+        return this.data
+    }
+
+    @Mutation 
+}
